d9e7caadf3b5c6ad0c0ad8afd1b1b661b95b94b7
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-02 21:33:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,3 +12,17 @@ def countdown(x)
    "HAPPY NEW YEAR!"
    
 end
+
+def countdown_with_sleep(x)
+  
+  while x > 0
+  
+    puts "#{x} SECOND(S)!"
+    x -= 1
+    sleep(1)
+  
+   end
+   
+   "HAPPY NEW YEAR!"
+  
+end
